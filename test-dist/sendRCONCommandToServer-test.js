845fc42f6578342154d992a9787029e5a03a40a7
csprance <chrissprance@gmail.com>
2017-04-28 04:11:47 -0400

    uncommented sendRcon tests all passing

--- test-dist/sendRCONCommandToServer-test.js
+++ test-dist/sendRCONCommandToServer-test.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; /* global it, describe, before, after */
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
+
 var _chai = require('chai');
 
 var _dist = require('../dist');
@@ -9,3 +12,41 @@ var _dist2 = _interopRequireDefault(_dist);
 var _secrets = require('../secrets');
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// make sure we can hit some endpoints
+describe('sendRCONCommandToServer', function () {
+  it('us77 status', function () {
+    return _dist2.default.sendRCONCommandToServer(_secrets.us77).then(function (res) {
+      (0, _chai.expect)(typeof res === 'undefined' ? 'undefined' : _typeof(res)).to.equal('string');
+    }).catch(function (e) {
+      throw e;
+    });
+  });
+
+  it('br1 status', function () {
+    return _dist2.default.sendRCONCommandToServer(_secrets.br1).then(function (res) {
+      (0, _chai.expect)(typeof res === 'undefined' ? 'undefined' : _typeof(res)).to.equal('string');
+    }).catch(function (e) {
+      throw e;
+    });
+  });
+});
+
+// make sure we can hit some endpoints again
+describe('sendRCONCommandToServer again', function () {
+  it('us77 status two', function () {
+    return _dist2.default.sendRCONCommandToServer(_secrets.us77).then(function (res) {
+      (0, _chai.expect)(typeof res === 'undefined' ? 'undefined' : _typeof(res)).to.equal('string');
+    }).catch(function (e) {
+      throw e;
+    });
+  });
+
+  it('br1 status two', function () {
+    return _dist2.default.sendRCONCommandToServer(_secrets.br1).then(function (res) {
+      (0, _chai.expect)(typeof res === 'undefined' ? 'undefined' : _typeof(res)).to.equal('string');
+    }).catch(function (e) {
+      throw e;
+    });
+  });
+});
--- test/sendRCONCommandToServer-test.js
+++ test/sendRCONCommandToServer-test.js
@@ -5,42 +5,40 @@ import {expect} from 'chai';
 import misrcon from '../dist';
 import {us77, br1} from '../secrets';
 
-//turn off these test for now
+// make sure we can hit some endpoints
+describe('sendRCONCommandToServer', () => {
+  it('us77 status', () => {
+    return misrcon.sendRCONCommandToServer(us77).then((res) => {
+      expect(typeof res).to.equal('string');
+    }).catch((e) => {
+      throw e;
+    });
+  });
 
-// // make sure we can hit some endpoints
-// describe('sendRCONCommandToServer', () => {
-//   it('us77 status', () => {
-//     return misrcon.sendRCONCommandToServer(us77).then((res) => {
-//       expect(typeof res).to.equal('string');
-//     }).catch((e) => {
-//       throw e;
-//     });
-//   });
-//
-//   it('br1 status', () => {
-//     return misrcon.sendRCONCommandToServer(br1).then((res) => {
-//       expect(typeof res).to.equal('string');
-//     }).catch((e) => {
-//       throw e;
-//     });
-//   });
-// });
-//
-// // make sure we can hit some endpoints again
-// describe('sendRCONCommandToServer again', () => {
-//   it('us77 status two', () => {
-//     return misrcon.sendRCONCommandToServer(us77).then((res) => {
-//       expect(typeof res).to.equal('string');
-//     }).catch((e) => {
-//       throw e;
-//     });
-//   });
-//
-//   it('br1 status two', () => {
-//     return misrcon.sendRCONCommandToServer(br1).then((res) => {
-//       expect(typeof res).to.equal('string');
-//     }).catch((e) => {
-//       throw e;
-//     });
-//   });
-// });
+  it('br1 status', () => {
+    return misrcon.sendRCONCommandToServer(br1).then((res) => {
+      expect(typeof res).to.equal('string');
+    }).catch((e) => {
+      throw e;
+    });
+  });
+});
+
+// make sure we can hit some endpoints again
+describe('sendRCONCommandToServer again', () => {
+  it('us77 status two', () => {
+    return misrcon.sendRCONCommandToServer(us77).then((res) => {
+      expect(typeof res).to.equal('string');
+    }).catch((e) => {
+      throw e;
+    });
+  });
+
+  it('br1 status two', () => {
+    return misrcon.sendRCONCommandToServer(br1).then((res) => {
+      expect(typeof res).to.equal('string');
+    }).catch((e) => {
+      throw e;
+    });
+  });
+});
