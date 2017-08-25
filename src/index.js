f3bdd931faa93c707a51452b32c6781c32f48495
csprance <chrissprance@gmail.com>
2017-08-25 02:36:12 -0400

    added flow types

--- src/index.js
+++ src/index.js
@@ -1,7 +1,41 @@
-export parseBanListResponseToJs from './parseBanListResponseToJs';
-export parseStatusResponseToJs from './parseStatusResponseToJs';
-export parseWhitelistResponseToJs from './parseWhitelistResponseToJs';
-export sendRCONCommandToServer from './sendRCONCommandToServer';
-export openConnection from './openConnection';
-export sendChainedCommand from './sendChainedCommand';
-export getAllServerData from './getAllServerData';
+// @flow
+
+import type {
+	SteamID as _SteamID,
+	CommandObject as _CommandObject,
+	Player as _Player,
+	PlayersArray as _PlayersArray,
+	ServerStatus as _ServerStatus,
+	PlayerStatus as _PlayerStatus,
+	StatusResponse as _StatusResponse,
+	BanListResponse as _BanListResponse,
+	WhiteListResponse as _WhiteListResponse,
+	AllData as _AllData
+} from './types';
+
+import _parseBanListResponseToJs from './parseBanListResponseToJs';
+import _parseStatusResponseToJs from './parseStatusResponseToJs';
+import _parseWhitelistResponseToJs from './parseWhitelistResponseToJs';
+import _sendRCONCommandToServer from './sendRCONCommandToServer';
+import _openConnection from './openConnection';
+import _sendChainedCommand from './sendChainedCommand';
+import _getAllServerData from './getAllServerData';
+
+export const parseBanListResponseToJs = _parseBanListResponseToJs;
+export const parseStatusResponseToJs = _parseStatusResponseToJs;
+export const parseWhitelistResponseToJs = _parseWhitelistResponseToJs;
+export const sendRCONCommandToServer = _sendRCONCommandToServer;
+export const openConnection = _openConnection;
+export const sendChainedCommand = _sendChainedCommand;
+export const getAllServerData = _getAllServerData;
+
+export type SteamID = _SteamID;
+export type CommandObject = _CommandObject;
+export type Player = _Player;
+export type PlayersArray = _PlayersArray;
+export type ServerStatus = _ServerStatus;
+export type PlayerStatus = _PlayerStatus;
+export type StatusResponse = _StatusResponse;
+export type BanListResponse = _BanListResponse;
+export type WhiteListResponse = _WhiteListResponse;
+export type AllData = _AllData;
